f24ed3a8975adf6f2ade696f2523b8e7f650bce4
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-08-03 20:56:40 +0530

    copy functionality

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,10 +5,26 @@ const SingleColor = ({ index, rgb, weight }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
-    <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">Copied to Clipboard</p>}
     </article>
   );
 };
