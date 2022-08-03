1b3b5ca1c59ada4e136575e3a03fd92fcbe3a314
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-08-03 19:55:37 +0530

    color generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,46 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [err, setErr] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setErr(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#f10525"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${err ? "error" : null}`}
+          />
+          <button className="btn">Generate</button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return <SingleColor key={index} index={index} {...color} />;
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
