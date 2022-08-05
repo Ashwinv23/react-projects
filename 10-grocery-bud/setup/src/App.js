024db99d40f5650ed5bdfc477a8b6ef608b97890
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-08-05 17:41:57 +0530

    added input and title

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [value, setValue] = useState("");
+
+  return (
+    <main>
+      <form>
+        <h3>Grocery Bud</h3>
+        <label htmlFor="inp"></label>
+        <input
+          type="text"
+          placeholder="Eg. eggs"
+          id="inp"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+      <List />
+    </main>
+  );
 }
 
-export default App
+export default App;
